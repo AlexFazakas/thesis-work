25bee24915c3760d18f103dc62930e962c7f8569
Alexandru Fazakas <alex.fazakas97@gmail.com>
2020-05-28 21:31:30 +0300

    display: start using the new user meant for scanning the table

--- display/dynamodb_query.py
+++ display/dynamodb_query.py
@@ -9,46 +9,8 @@ import sys
 import json
 import click
 
-INEXISTENT_FIELD = 1
-
 app = Flask(__name__)
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('reports')
 data = []
-
-DB_FIELDS = {
-    'date': 'Date',
-    'architecture': 'Architecture',
-    'disassembly':'Disassembly',
-    'distrorelease': 'DistroRelease',
-    'executablepath': 'ExecutablePath',
-    'executabletimestamp': 'ExecutableTimestamp',
-    'package': 'Package',
-    'problemtype': 'ProblemType',
-    'proccmdline': 'ProcCmdline',
-    'proccwd': 'ProcCwd',
-    'procmaps': 'ProcMaps',
-    'procstatus': 'ProcStatus',
-    'registers': 'Registers',
-    'signal': 'Signal',
-    'stacktrace': 'Stacktrace',
-    'stacktraceaddresssignature': 'StacktraceAddressSignature',
-    'stacktracetop': 'StacktraceTop',
-    'threadstacktrace': 'ThreadStacktrace',
-    'uname': 'Uname',
-    'usergroups': 'UserGroups',
-    'ip': 'Source IP Address'
-}
-
-def parse_fields(raw_fields: list):
-    new_fields = []
-    for field in raw_fields:
-        if field.lower() in DB_FIELDS:
-            new_fields.append(DB_FIELDS[field.lower()])
-        else:
-            print('Field \'{}\' does not match any fields in the database!'.format(field))
-            sys.exit(INEXISTENT_FIELD)
-    return new_fields if new_fields else list(DB_FIELDS.values())
 
 @app.route('/raw_report', methods=['GET'])
 def raw_report():
@@ -63,13 +25,16 @@ def print_table():
 
     return render_template('table.html')
 
-@click.command()
-@click.option('--fields', '-f', multiple=True)
-def main(fields: (str)):
+def main():
     global data
 
-    fields = parse_fields(fields)
-    data = table.scan(AttributesToGet=fields)['Items']
+    with open ('ui_credentials.json', 'r') as f:
+        credentials = json.load(f)
+    client = boto3.client('dynamodb',
+                          aws_access_key_id=credentials['AccessKeyId'],
+                          aws_secret_access_key=credentials['SecretAccessKey']
+                        )
+    data = client.scan(TableName='reports')['Items']
     file_loader = FileSystemLoader('./templates')
     env = Environment(loader=file_loader)
     template = env.get_template('table.j2')
